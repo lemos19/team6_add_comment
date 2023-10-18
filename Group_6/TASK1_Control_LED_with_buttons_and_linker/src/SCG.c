11e560c0659ca3db39ee9750b83cd01fa653cc54
Duocdt2605 <duocdt.2605@gmail.com>
2023-10-18 13:43:07 +0700

    add comment for SCG.c module

--- Group_6/TASK1_Control_LED_with_buttons_and_linker/src/SCG.c
+++ Group_6/TASK1_Control_LED_with_buttons_and_linker/src/SCG.c
@@ -1,4 +1,76 @@
+/******************************************************************************
+ * Project Name: FRESHER ASSIGNMENT
+ * File Name: SCG.c
+ *
+ * Description: Implementation of the SCG module
+ *              Target systems:           S32K144
+ *              Derivatives:              ARM Cortex M4
+ *              Compiler:                 S32DS
+ *
+ *****************************************************************************/
+
+ /**
+  *   @file       SCG_Type.h
+  *   @version    V0.1
+  *   @brief      V0.1 SCG Driver
+  *   @addtogroup SCG_driver
+  */
+
+/******************************************************************************
+ *  INCLUDES
+ *****************************************************************************/
 #include "SCG_Type.h"
+
+/******************************************************************************
+ *  DEFINES & MACROS
+ *****************************************************************************/
+/**
+ * @brief  xxxxxxxx1
+ */
+
+/******************************************************************************
+ *  EXTERN
+ *****************************************************************************/
+
+
+/******************************************************************************
+ *  LOCAL TYPEDEFS
+ *****************************************************************************/
+/**
+* @brief  Template TestType1 brief.
+*/
+
+/******************************************************************************
+ *  GLOBAL VARIABLES
+ *****************************************************************************/
+/**
+ * @brief  test brief2
+ */
+
+/******************************************************************************
+ *  LOCAL FUNCTION PROTOTYPES
+ *****************************************************************************/
+
+/******************************************************************************
+ *  LOCAL FUNCTION
+ ******************************************************************************/
+
+/******************************************************************************
+ *  GLOBAL FUNCTION
+ *****************************************************************************/
+
+ /**
+  *   @brief      This is config FIRC, SOSC, SIRC, SPLL Div2 for peripherals.
+  *				  The valid configuration variable.
+  *
+  *   @param[in]  Source		    source
+  *   @param[in]  Divide_Options	div
+  *
+  *   @return     Void			    None
+  *
+  *   @note       None.
+  *
+ */
 void SCG_Asyn_Peripheral_Sources(Source source,Divide_Options div){
     if (source == FIRCDIV2_CLK){
         SCG->SCG_FIRCDIV.Fields.Div2 = div;
@@ -13,12 +85,38 @@ void SCG_Asyn_Peripheral_Sources(Source source,Divide_Options div){
         SCG->SCG_SPLLDIV.Fields.Div2 = div;
     }
 }
+
+/**
+ *   @brief      This is config SOSC clock source
+ *				 The valid configuration variable.
+ *
+ *   @param[in]  None
+ *
+ *   @return     Void			None
+ *
+ *   @note       None.
+ *
+*/
 void Config_SOSC_CLK(void){
     SCG->SCG_SOSCCSR = CLEAR_BIT(SCG->SCG_SOSCCSR,0);
     SCG->SCG_SOSCCFG = SET_BIT(SCG->SCG_SOSCCFG,2)|SET_BIT(SCG->SCG_SOSCCFG,3)|SET_BIT(SCG->SCG_SOSCCFG,5);
     SCG->SCG_SOSCCSR = SET_BIT(SCG->SCG_SOSCCSR,0);
     while (!(GET_STATE(SCG->SCG_SOSCCSR,24))){}
 }
+
+/**
+ *   @brief      This is config SPLL clock source
+ *				 The valid configuration variable.
+ *
+ *   @param[in]  Clock_Source_t     Sosc_Firc
+ *   @param[in]  PreDiv_SPLL        div
+ *   @param[in]  Multiply_SPLL      mul
+ *
+ *   @return     Void			None
+ *
+ *   @note       None.
+ *
+*/
 void Config_SPLL_CLK(Clock_Source_t Sosc_Firc,PreDiv_SPLL div,Multiply_SPLL mul){
     SCG->SCG_SPLLCSR = CLEAR_BIT(SCG->SCG_SPLLCSR,0);
     if (Sosc_Firc == SOSC){
@@ -33,6 +131,24 @@ void Config_SPLL_CLK(Clock_Source_t Sosc_Firc,PreDiv_SPLL div,Multiply_SPLL mul)
     SCG->SCG_SPLLCSR = SET_BIT(SCG->SCG_SPLLCSR,0);
     while (!(GET_STATE(SCG->SCG_SPLLCSR,24))){}
 }
+
+/**
+ *   @brief      This is config mode RCCR clock source
+ *				 The valid configuration variable.
+ *
+ *   @param[in]  Clock_Source_t     source
+ *   @param[in]  uint8_t            divCore1_16_minus1
+ *   @param[in]  uint8_t            divBus1_16_minus1
+ *   @param[in]  uint8_t            divSlow1_8_minus1
+ * 
+ *   @arg        CCR_t              tempRCCR
+ *   @arg        tempRCCR           Register
+ *
+ *   @return     Void			    None
+ *
+ *   @note       None.
+ *
+*/
 void Config_RCCR(Clock_Source_t source,uint8_t divCore1_16_minus1,uint8_t divBus1_16_minus1,uint8_t divSlow1_8_minus1){
     CCR_t tempRCCR;
     tempRCCR.Register = 0;
--- Group_6/TASK1_Control_LED_with_buttons_and_linker/src/SIM.c
+++ Group_6/TASK1_Control_LED_with_buttons_and_linker/src/SIM.c
@@ -72,7 +72,7 @@
  */
 #include "SIM_Type.h"
 void Clkout_config(Clkoutsource source,Clkoutdiv_t div){
-	SIM->CHIPCTL.Fields.CLKOUTEN = Disable;
+    SIM->CHIPCTL.Fields.CLKOUTEN = Disable;
     SIM->CHIPCTL.Fields.CLKOUTSEL=  source; 
     SIM->CHIPCTL.Fields.CLKOUTDIV= div;
     SIM->CHIPCTL.Fields.CLKOUTEN = Enable; 
